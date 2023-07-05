34d700925241bda917b423652df0c28cac94a6a7
ErikRospo <rerik.work@gmail.com>
2023-07-05 14:32:03 -0700

    moved assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  build:{outDir:"docs/"}
+  build:{outDir:"docs/",assetsDir:""}
 })
